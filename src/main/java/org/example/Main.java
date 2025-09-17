a78fcc72d91e95ebab81d9631be7d7fd5bd32b4f
imanezahri002 <imanezh5@gmail.com>
2025-09-17 11:39:51 +0100

    structure et menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,42 @@
 package org.example;
 
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private static boolean isRunning = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (isRunning) {
+            displayAuthMenu();
+        }
+
+        scanner.close();
+        System.out.println("Programme terminé.");
+    }
+
+    private static void displayAuthMenu() {
+        System.out.println("\n=== ConsoleBank ===");
+        System.out.println("1. Register");
+        System.out.println("2. Login");
+        System.out.println("0. Exit");
+        System.out.print("Choix : ");
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+            case 1 -> System.out.println("Fonction d'inscription...");
+            case 2 -> {
+                System.out.println("Fonction de connexion...");
+            }
+            case 0 -> {
+                System.out.println("Exit");
+                isRunning = false;
+            }
+            default -> System.out.println("Choix invalide !");
         }
     }
 }
---
+++ src/main/java/org/example/models/User.java
@@ -0,0 +1,12 @@
+package org.example.models;
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private  String fullname;
+    private String email;
+    private String adresse;
+    public User(String name, String email, String phone) {
+
+    }
+}
